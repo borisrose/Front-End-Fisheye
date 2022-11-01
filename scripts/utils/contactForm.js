c783b19fd0967eae3095a81d5b5997cc828fb46b
boralex <boris.alexandre.rose@gmail.com>
2022-11-01 13:03:39 +0100

    "feat/modal"

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,177 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+    const submitButton = document.getElementById('contact-button');
+    submitButton.setAttribute('disabled', true)
+    submitButton.classList.add('forbidden-submit');
+	modal.style.display = "flex";
+
+    class Input {
+
+
+        constructor(domEl,pattern, errorMessage, isValid){
+
+            this.domEl = domEl;
+            this.pattern = pattern;
+            this.errorMessage = errorMessage;
+            this.isValid = false;
+
+        };
+
+
+        displayErrorMessage = (specificErrorMessage, valid) => {
+           
+                let siblings = Array.from(this.domEl.parentNode.childNodes);
+                let para = siblings.filter(n => n.nodeName === 'P')[0];     
+                if(valid === true){
+                    para.innerHTML = "";
+                }
+                else {
+                    para.innerText = specificErrorMessage;
+                }
+              
+          
+        }
+
+        displayColorMessage = (valid) => {
+            if(valid === true){
+                if(Array.from(this.domEl.classList).filter(c => c === 'non-valid').length > 0){
+                    this.domEl.classList.remove('non-valid');
+                }
+                this.domEl.classList.add('valid');
+            }
+            else {
+                if(Array.from(this.domEl.classList).filter(c => c === 'valid').length > 0){
+                    this.domEl.classList.remove('valid');
+                }
+                this.domEl.classList.add('non-valid')
+             
+            }
+           
+        }
+
+        formChecker = (inputsArray) => {
+            if(inputsArray.find(input => input.isValid === false)){
+                submitButton.setAttribute('disabled', true)
+                if(Array.from(submitButton.classList).find(c => c === 'allowed-submit')){
+                    submitButton.classList.remove('allowed-submit');
+                }
+                if(!Array.from(submitButton.classList).find(c => c === 'forbidden-submit')){
+                    submitButton.classList.add('forbidden-submit');
+                }
+       
+            }
+            else {
+                submitButton.setAttribute('disabled', false) 
+                if(!Array.from(submitButton.classList).find(c => c === 'allowed-submit')){
+                    submitButton.classList.add('allowed-submit');
+                }
+                if(Array.from(submitButton.classList).find(c => c === 'forbidden-submit')){
+                    submitButton.classList.remove('forbidden-submit');
+                }
+            }
+        }
+
+        patternChecker = (inputsArray) => {
+            this.domEl.addEventListener('change', (e)=> {
+                let valid = this.pattern.test(e.target.value)
+                if(valid){  
+                    this.displayColorMessage(valid)
+                    this.displayErrorMessage("", valid);
+                    this.isValid = true
+                    this.formChecker(inputsArray);
+       
+                } else {
+                    if( (this.domEl.id === 'lastname' || this.domEl.id === 'firstname') && e.target.value.length < 2 && e.target.value.length > 0){
+                        this.displayErrorMessage(this.errorMessage.invalidAnswer, valid);
+                    }
+                    else if((this.domEl.id === 'email' || this.domEl.id === 'message') && e.target.value.length > 0){
+                        this.displayErrorMessage(this.errorMessage.invalidAnswer, valid)
+                    }
+                    else {
+                        this.displayErrorMessage(this.errorMessage.noAnswer, valid);
+                    }
+                    this.displayColorMessage(valid)
+                    this.isValid = false
+                    this.formChecker(inputsArray);
+                }
+             
+            })
+        }
+
+
+    }
+
+
+    const instancesToCheck = () => {
+
+    let firstname;
+    let lastname;
+    let email;
+    let message;  
+
+    return ({
+
+        firstname : new Input(
+            document.getElementById('firstname'),
+            /^([a-zéèöà]{2,})((([-])([a-z]{2,}))?)$/i,
+            {noAnswer: 'entrez une réponse', invalidAnswer:'deux caractères minimum'},     
+        ),
+
+        lastname : new Input(
+            document.getElementById('lastname'),
+            /^([a-z-àéèö]{2,})((([-])([a-z]{2,}))?)$/i,
+            {noAnswer: 'entrez une réponse', invalidAnswer:'deux caractères minimum'},
+            
+        ),
+
+        email : new Input(
+            document.getElementById('email'),
+            /^[a-z\.]{2,}[@]{1}[a-z0-9\-]{3,}[\.]{1}[a-z]{2,5}$/,
+            {noAnswer: 'entrez une réponse', invalidAnswer:'email non valide'},    
+        ),
+
+        message : new Input(
+            document.getElementById('message'),
+            /^[\s\S-àèéö]{0,200}$/ig,
+            {noAnswer: 'entrez une réponse', invalidAnswer:'message non valide'},    
+        ),
+
+
+
+        })
+       
+
+    }
+
+
+    const {firstname, lastname, email, message } = instancesToCheck();
+
+    inputsArray = [firstname, lastname, email, message];
+
+    for(let input of inputsArray){
+   
+        input.patternChecker(inputsArray);
+    
+
+    }
+
+ 
+    
+
+ 
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
+
+    let firstname = document.getElementById('firstname');
+    let lastname = document.getElementById('lastname');
+    let email = document.getElementById('email');
+    let message = document.getElementById('message');
+
+    for(let el of [firstname, lastname, email, message]){;
+        el.value = "";
+    }
+
 }
